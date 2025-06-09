123482bc265330b1975493cbcb67f5779e92c73f
wen952888 <wenxiu9528@outlook.com>
2025-06-09 10:43:34 +0800

    Delete api directory

--- api/ai_host.php
+++
@@ -1,5 +0,0 @@
-<?php
-// declare(strict_types=1);
-// header('Content-Type: application/json; charset=utf-8');
-// echo json_encode(['message' => 'AI Host endpoint - currently not in primary use. AI logic is integrated into submit_hand.php flow.']);
-?>
